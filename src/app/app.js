a67d5e22fe099aff2462deb89191d2d58691e99a
khaledrefaat <khaledrefaat08@gmail.com>
2020-03-31 22:15:35 +0200

    webpack installed

---
+++ src/app/app.js
@@ -0,0 +1,21 @@
+const alertService = new AlertService();
+const componentService = new ComponentService();
+
+const run = (alertService, componentService) => {
+  alertService.hideErrors();
+
+
+  componentService.onClick(() => {
+
+    hideErrors();
+    const parsedInputs = parseInputs(...inputs);
+    if (inputsAreValid(...parsedInputs)) {
+      const [numA, numB] = parsedInputs;
+      resultDiv.innerText = numA + numB;
+    } else {
+      resultDiv.innerText = "";
+      handleAdditionError(inputs, parsedInputs);
+    }
+  });
+}
+run(alertService, componentService);
---
+++ src/app/component.service.js
@@ -0,0 +1,21 @@
+class ComponentService {
+    constructor() {
+        this.numberOneInput = document.getElementById("numberOne");
+        this.numberTwoInput = document.getElementById("numberTwo");
+        this.addValuesButton = document.getElementById("addValues");
+        this.resultDiv = document.getElementById("result");
+    }
+
+    getInput() {
+        return [this.numberOneInput.value, this.numberTwoInput.value]
+    }
+
+    setResult(str) {
+        this.resultDiv.innerTEXT = str;
+    }
+
+    onClick(cb) {
+        this.addValuesButton.addEventListener('click', cb);
+    }
+
+}
